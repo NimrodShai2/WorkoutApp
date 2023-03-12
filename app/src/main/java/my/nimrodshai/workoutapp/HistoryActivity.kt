122441095ee26c584b1c3c9c80a839ce84e5e591
nimni <nimni@LAPTOP-N14LBO6S>
2023-03-12 15:06:27 +0200

    Added History RecyclerView and UI

--- app/src/main/java/my/nimrodshai/workoutapp/HistoryActivity.kt
+++ app/src/main/java/my/nimrodshai/workoutapp/HistoryActivity.kt
@@ -2,7 +2,13 @@ package my.nimrodshai.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.coroutines.launch
 import my.nimrodshai.workoutapp.databinding.ActivityHistoryBinding
+import my.nimrodshai.workoutapp.db.HistoryDao
+import my.nimrodshai.workoutapp.db.WorkoutApp
 
 class HistoryActivity : AppCompatActivity() {
     private var binding: ActivityHistoryBinding? = null
@@ -17,5 +23,36 @@ class HistoryActivity : AppCompatActivity() {
         binding?.tbHistory?.setNavigationOnClickListener {
             onBackPressed()
         }
+        val dao = (application as WorkoutApp).db.historyDao()
+        getAllDates(dao)
+    }
+
+    private fun getAllDates(historyDao: HistoryDao) {
+        lifecycleScope.launch {
+            historyDao.getAllDates().collect {
+                if (it.isNotEmpty()) {
+                    binding?.tvHistory?.visibility = View.VISIBLE
+                    binding?.rvHistory?.visibility = View.VISIBLE
+                    binding?.tvNoDataAvailable?.visibility = View.INVISIBLE
+
+                    binding?.rvHistory?.layoutManager = LinearLayoutManager(this@HistoryActivity)
+                    val dates = ArrayList<String>()
+                    for (date in it) {
+                        dates.add(date.date)
+                    }
+                    val adapter = HistoryAdapter(dates)
+                    binding?.rvHistory?.adapter = adapter
+                } else {
+                    binding?.tvHistory?.visibility = View.GONE
+                    binding?.rvHistory?.visibility = View.GONE
+                    binding?.tvNoDataAvailable?.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
--- app/src/main/java/my/nimrodshai/workoutapp/db/HistoryDao.kt
+++ app/src/main/java/my/nimrodshai/workoutapp/db/HistoryDao.kt
@@ -2,9 +2,14 @@ package my.nimrodshai.workoutapp.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface HistoryDao {
     @Insert
     suspend fun insert (historyEntity: HistoryEntity)
+
+    @Query("SELECT * FROM `history-table`")
+    fun getAllDates(): Flow<List<HistoryEntity>>
 }
