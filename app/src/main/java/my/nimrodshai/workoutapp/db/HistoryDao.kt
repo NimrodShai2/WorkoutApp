7e2c5c9c3d7c2b11ef1de7d043ab8c0b412adc62
nimni <nimni@LAPTOP-N14LBO6S>
2023-03-12 12:54:32 +0200

    Set Up room and DAO for history database

---
+++ app/src/main/java/my/nimrodshai/workoutapp/db/HistoryDao.kt
@@ -0,0 +1,10 @@
+package my.nimrodshai.workoutapp.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+
+@Dao
+interface HistoryDao {
+    @Insert
+    suspend fun insert (historyEntity: HistoryEntity)
+}
